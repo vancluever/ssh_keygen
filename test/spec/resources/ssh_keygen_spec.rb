e9e6fc29ccaaeb7532b4246f1586617939c35974
Chris Marchesi <inbox@vancluevertech.com>
2015-11-13 09:47:35 -0800

    Restoring and re-arranging all tests

--- test/spec/resources/ssh_keygen_spec.rb
+++ test/spec/resources/ssh_keygen_spec.rb
@@ -2,61 +2,64 @@ require 'spec_helper'
 
 describe SSHKeygen::Resource do
   step_into(:ssh_keygen)
-  step_into(:chef_gem)
-  recipe do
-    ssh_keygen '/root/.ssh/id_rsa' do
-      action :create
+
+  context 'base tests without passphrase' do
+    recipe do
+      ssh_keygen '/root/.ssh/id_rsa' do
+        action :create
+        secure_directory true
+      end
     end
 
-    # ssh_keygen '/root/.ssh/id_rsa_encrypted' do
-    #   action :create
-    #   passphrase 'ilikerandompasswordsbutthiswilldo'
-    # end
+    it { is_expected.to create_file('/root/.ssh/id_rsa') }
+
+    it 'creates a file at /root/.ssh/id_rsa with the proper permissions' do
+      expect(chef_run).to create_file('/root/.ssh/id_rsa').with(
+        user:   'root',
+        group:  'root',
+        mode:   0600
+      )
+    end
+
+    it 'creates a file at /root/.ssh/id_rsa.pub with the proper permissions' do
+      expect(chef_run).to create_file('/root/.ssh/id_rsa.pub').with(
+        user:   'root',
+        group:  'root',
+        mode:   0600
+      )
+    end
+
+    it 'secures the /root/.ssh directory' do
+      expect(chef_run).to create_directory('/root/.ssh').with(
+        user:   'root',
+        group:  'root',
+        mode:   0700
+      )
+    end
   end
 
-  it { is_expected.to create_file('/root/.ssh/id_rsa') }
+  context 'passphrase-specific tests' do
+    recipe do
+      ssh_keygen '/root/.ssh/id_rsa_encrypted' do
+        action :create
+        passphrase 'ilikerandompasswordsbutthiswilldo'
+      end
+    end
 
-  # it 'installs the sshkey gem thru chef_gem' do
-  #   expect(chef_run).to install_chef_gem('sshkey')
-  # end
-  #
-  # it 'creates a file at /root/.ssh/id_rsa with the proper permissions' do
-  #   expect(chef_run).to create_file('/root/.ssh/id_rsa').with(
-  #     user:   'root',
-  #     group:  'root',
-  #     mode:   0600
-  #   )
-  # end
-  #
-  # it 'creates a file at /root/.ssh/id_rsa.pub with the proper permissions' do
-  #   expect(chef_run).to create_file('/root/.ssh/id_rsa.pub').with(
-  #     user:   'root',
-  #     group:  'root',
-  #     mode:   0600
-  #   )
-  # end
-  #
-  # it 'creates a file at /root/.ssh/id_rsa_encrypted with the proper permissions' do
-  #   expect(chef_run).to create_file('/root/.ssh/id_rsa_encrypted').with(
-  #     user:   'root',
-  #     group:  'root',
-  #     mode:   0600
-  #   )
-  # end
-  #
-  # it 'creates a file at /root/.ssh/id_rsa_encrypted with the proper permissions' do
-  #   expect(chef_run).to create_file('/root/.ssh/id_rsa_encrypted.pub').with(
-  #     user:   'root',
-  #     group:  'root',
-  #     mode:   0600
-  #   )
-  # end
-  #
-  # it 'secures the /root/.ssh directory' do
-  #   expect(chef_run).to create_directory('/root/.ssh').with(
-  #     user:   'root',
-  #     group:  'root',
-  #     mode:   0700
-  #   )
-  # end
+    it 'creates a file at /root/.ssh/id_rsa_encrypted with the proper permissions' do
+      expect(chef_run).to create_file('/root/.ssh/id_rsa_encrypted').with(
+        user:   'root',
+        group:  'root',
+        mode:   0600
+      )
+    end
+
+    it 'creates a file at /root/.ssh/id_rsa_encrypted.pub with the proper permissions' do
+      expect(chef_run).to create_file('/root/.ssh/id_rsa_encrypted.pub').with(
+        user:   'root',
+        group:  'root',
+        mode:   0600
+      )
+    end
+  end
 end
