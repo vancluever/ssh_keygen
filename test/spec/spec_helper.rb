ad4381e8368c248c9175649778d1ce98315eae82
Chris Marchesi <inbox@vancluevertech.com>
2015-11-14 15:47:27 -0800

    Removing spec_helper stuff that is no longer needed (using poise-boiler now)

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,10 +1,2 @@
 require 'poise_boiler/spec_helper'
 require 'ssh_keygen'
-
-# require 'chefspec'
-# require 'halite/spec_helper'
-# require 'ssh_keygen'
-
-# RSpec.configure do |config|
-#   config.include Halite::SpecHelper
-# end
