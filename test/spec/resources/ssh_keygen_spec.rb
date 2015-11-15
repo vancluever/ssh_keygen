1773e8a95ea888e4557c07112a8c465e9eca1c4f
Chris Marchesi <inbox@vancluevertech.com>
2015-11-14 17:51:39 -0800

    SSHKeygen::Generator coverage

--- test/spec/resources/ssh_keygen_spec.rb
+++ test/spec/resources/ssh_keygen_spec.rb
@@ -1,4 +1,43 @@
+# Copyright 2015 Chris Marchesi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'spec_helper'
+
+describe SSHKeygen::Generator do
+  context 'default options with a bit strength of 2048' do
+    key = ::SSHKeygen::Generator.new(2048, 'rsa', nil, 'test@rspec')
+
+    it 'has a valid PEM-encoded private key' do
+      generator_key_digest = key.key_fingerprint
+      validator_key_digest = create_fingerprint_from_key(key.private_key)
+
+      expect(generator_key_digest).to eq(validator_key_digest)
+    end
+
+    it 'has a valid OpenSSH-style formatted-public key' do
+      expect(key.ssh_public_key).to match(%r{^ssh-rsa [a-zA-Z0-9=/+]+ test@rspec})
+    end
+
+    it 'has a valid public key for the private key' do
+      generator_key_digest = key.key_fingerprint
+      public_key = key.ssh_public_key.split(' ')[1]
+      public_key_digest = create_fingerprint_from_key(Base64.strict_decode64(public_key))
+
+      expect(generator_key_digest).to eq(public_key_digest)
+    end
+  end
+end
 
 describe SSHKeygen::Resource do
   step_into(:ssh_keygen)
--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,2 +1,24 @@
+# Copyright 2015 Chris Marchesi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'poise_boiler/spec_helper'
 require 'ssh_keygen'
+
+# a small helper function that creates a SHA1 fingerprint from a private or
+# public key.
+def create_fingerprint_from_key(key)
+  new_key = OpenSSL::PKey::RSA.new(key)
+  new_key_digest = OpenSSL::Digest::SHA1.new(new_key.public_key.to_der).to_s.scan(/../).join(':')
+  new_key_digest
+end
